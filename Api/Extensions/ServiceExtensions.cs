394a78a893eff2f7824e2c2d1a58556b033b9abf
Francisco Josafá <josafa.lopes@inventione.com.br>
2024-04-19 23:55:51 -0300

    Cria estrutura do projeto e configura serviço de logging

---
+++ Api/Extensions/ServiceExtensions.cs
@@ -0,0 +1,11 @@
+﻿using Domain.Interfaces.Services;
+using LoggerService;
+
+namespace Api.Extensions
+{
+    public static class ServiceExtensions
+    {
+        public static void ConfigureLoggerService(this IServiceCollection services) =>
+            services.AddSingleton<ILoggerManager, LoggerManager>();
+    }
+}
---
+++ Api/Program.cs
@@ -0,0 +1,41 @@
+
+using Api.Extensions;
+using NLog;
+
+namespace Api
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            LogManager.Setup().LoadConfigurationFromFile(string.Concat(Directory.GetCurrentDirectory(), "/nlog.config"));
+
+            // Add services to the container.
+            builder.Services.ConfigureLoggerService();
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
