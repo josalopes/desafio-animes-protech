4b444101abfbd14554a26c84c11c06b4acae534b
Francisco Josafá <josafa.lopes@inventione.com.br>
2024-04-21 07:11:51 -0300

    Cria DbContext para efetuar as migrations para criação do database e tabelas.

--- Infrastructure/Database/ApplicationDbContext.cs
+++ Infrastructure/Database/ApplicationDbContext.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Entities.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,13 @@ using System.Threading.Tasks;
 
 namespace Infrastructure.Database
 {
-    internal class ApplicationDbContext
+    public class ApplicationDbContext(DbContextOptions options) : DbContext(options)
     {
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+
+        public DbSet<Anime> Animes { get; set; }
     }
 }
