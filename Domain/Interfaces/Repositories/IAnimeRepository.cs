49b531d0b24e3a11093d8280e4de760a37190190
Francisco Josafá <josafa.lopes@inventione.com.br>
2024-04-21 07:13:39 -0300

    Implementa as interfaces do padrão repository

---
+++ Domain/Interfaces/Repositories/IAnimeRepository.cs
@@ -0,0 +1,13 @@
+﻿using Entities.Models;
+
+namespace Domain.Interfaces.Repositories
+{
+    public interface IAnimeRepository
+    {
+        IEnumerable<Anime> GetAllAnimes(bool trackChanges);
+
+        Anime GetAnime(Guid id, bool trackChanges);
+
+        void CreateAnime(Anime anime);
+    }
+}
---
+++ Domain/Interfaces/Repositories/IRepositoryManager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Interfaces.Repositories
+{
+    public interface IRepositoryManager
+    {
+        IAnimeRepository Anime { get; }
+
+        void Save();
+    }
+}
